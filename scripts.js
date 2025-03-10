875bef3b98807071ead9f1766a24d9e78a271c76
Will VanderWyden <vanderwyden@gmail.com>
2025-03-11 08:35:56 +0900

    full video toggles

--- scripts.js
+++ scripts.js
@@ -1,25 +1,26 @@
 // get our elements
 const player = document.querySelector(".player");
 const playButton = player.querySelector(".toggle");
-const videoPlaying = player.querySelector(".viewer");
+const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress__filled");
 const progressBar = player.querySelector(".viewer");
 const volume = player.querySelector(".slider");
-const skipButtons = player.querySelectorAll(".[data-skip]");
+const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 
 
 
 // make our functions
 function playVideo() {
-    if (videoPlaying.paused === true) {
-        videoPlaying.play()
+    if (video.paused === true) {
+        video.play()
     }
     else {
-        videoPlaying.pause()
+        video.pause()
     }
 };
 
 
 // hook up our event listeners
 playButton.addEventListener('click', playVideo);
+video.addEventListener('click', playVideo);
