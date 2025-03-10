d59b0b92698d37ab0fd09689a785cf325b9cba9a
Will VanderWyden <vanderwyden@gmail.com>
2025-03-11 08:18:18 +0900

    play button working

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
 // get our elements
+player = document.querySelector(".player");
+playButton = document.querySelector(".player__button");
+videoPlaying = document.querySelector(".player__video");
 
 
 
 // make our functions
-
+function playVideo() {
+    if (videoPlaying.paused === true) {
+        videoPlaying.play()
+    }
+    else {
+        videoPlaying.pause()
+    }
+};
 
 
 // hook up our event listeners
+playButton.addEventListener('click', playVideo);
